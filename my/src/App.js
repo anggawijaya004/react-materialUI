05b766a61e79ebd3a23ce69f9552e54d5a0a249f
anggawijaya004 <anggawijaya004@gmail.com>
2020-09-05 14:10:43 +0700

    oo

---
+++ my/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+import Login from './pages/Login';
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
+function App() {
+
+  const theme = createMuiTheme({
+    typography: {
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+    },
+  });
+
+  return (
+    <Router>
+    <ThemeProvider theme={theme}>
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+      </Switch>
+    </div>
+    </ThemeProvider>
+    </Router>
+  );
+}
+
+export default App;
